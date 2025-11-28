7b565975967bbf48eea909928d7efc261b879add
Mateusz Zając <matzayonc@gmail.com>
2025-11-28 18:31:55 +0100

    compiling correctly

--- kivis/src/transaction.rs
+++ kivis/src/transaction.rs
@@ -1,17 +1,14 @@
-use bincode::{config::Configuration, serde::encode_to_vec};
-
 #[cfg(feature = "atomic")]
 use crate::traits::AtomicStorage;
 use crate::{
-    wrap::{encode_value, wrap, Subtable, WrapPrelude},
+    wrap::{wrap, Subtable, WrapPrelude},
     Database, DatabaseEntry, DatabaseError, DeriveKey, Incrementable, Manifest, Manifests,
-    RecordKey, SimpleIndexer, Storage, StorageInner, Unifier,
+    RecordKey, SimpleIndexer, Storage, Unifier, UnifierData,
 };
 
 #[cfg(not(feature = "std"))]
 use alloc::vec::Vec;
 use core::marker::PhantomData;
-type Write = (Vec<u8>, Vec<u8>);
 
 /// A database transaction that accumulates low-level byte operations (writes and deletes)
 /// without immediately applying them to storage.
@@ -19,22 +16,22 @@ type Write = (Vec<u8>, Vec<u8>);
 /// This struct is always available, but the `commit` method is only available when the "atomic" feature is enabled.
 pub struct DatabaseTransaction<Manifest, U: Unifier> {
     /// Pending write operations: (key, value) pairs
-    pending_writes: Vec<Write>,
+    pending_writes: Vec<(U::D, U::D)>,
     /// Pending delete operations: keys to delete
-    pending_deletes: Vec<Vec<u8>>,
+    pending_deletes: Vec<U::D>,
     /// Serialization configuration
     serialization_config: U,
     _marker: PhantomData<Manifest>,
 }
 
-impl<M: Manifest, U: Unifier + Clone> DatabaseTransaction<M, U> {
+impl<M: Manifest, U: Unifier + Copy> DatabaseTransaction<M, U> {
     /// Creates a new empty transaction. Should be used by [`Database::create_transaction`].
     pub fn new<S: Storage<Serializer = U>>(database: &Database<S, M>) -> Self {
         Self {
             pending_writes: Vec::new(),
             pending_deletes: Vec::new(),
             // TODO: Consider referencing, instead of cloning.
-            serialization_config: database.serialization_config().clone(),
+            serialization_config: database.serialization_config,
             _marker: PhantomData,
         }
     }
@@ -57,6 +54,7 @@ impl<M: Manifest, U: Unifier + Clone> DatabaseTransaction<M, U> {
     where
         R: DeriveKey<Key = K> + DatabaseEntry<Key = K>,
         M: Manifests<R>,
+        SimpleIndexer<U>: crate::traits::Indexer<Error = U::SerError>,
     {
         let original_key = R::key(record);
         let writes = self.prepare_writes::<R>(record, &original_key)?;
@@ -70,14 +68,15 @@ impl<M: Manifest, U: Unifier + Clone> DatabaseTransaction<M, U> {
     ///
     /// Returns a [`DatabaseError`] if writing to the underlying storage fails.
     ///
-    pub fn put<S: Storage, R: DatabaseEntry>(
+    pub fn put<S: Storage<Serializer = U>, R: DatabaseEntry>(
         &mut self,
         record: &R,
         database: &mut Database<S, M>,
-    ) -> Result<R::Key, DatabaseError<S::StoreError>>
+    ) -> Result<R::Key, DatabaseError<S>>
     where
         R::Key: RecordKey<Record = R> + Incrementable + Ord,
         M: Manifests<R>,
+        SimpleIndexer<U>: crate::traits::Indexer<Error = U::SerError>,
     {
         let last_key = database.manifest.last();
         let new_key = if let Some(ref k) = last_key {
@@ -86,7 +85,9 @@ impl<M: Manifest, U: Unifier + Clone> DatabaseTransaction<M, U> {
             Default::default()
         };
 
-        let writes = self.prepare_writes::<R>(record, &new_key)?;
+        let writes = self
+            .prepare_writes::<R>(record, &new_key)
+            .map_err(DatabaseError::Serialization)?;
         for (k, v) in writes {
             self.write(k, v);
         }
@@ -101,6 +102,7 @@ impl<M: Manifest, U: Unifier + Clone> DatabaseTransaction<M, U> {
     where
         R::Key: RecordKey<Record = R>,
         M: Manifests<R>,
+        SimpleIndexer<U>: crate::traits::Indexer<Error = U::SerError>,
     {
         let deletes = self.prepare_deletes::<R>(record, key)?;
         for d in deletes {
@@ -113,7 +115,7 @@ impl<M: Manifest, U: Unifier + Clone> DatabaseTransaction<M, U> {
     ///
     /// If the same key is written multiple times, only the last value is kept.
     /// If a key is both written and deleted, the write takes precedence.
-    fn write(&mut self, key: Vec<u8>, value: Vec<u8>) {
+    fn write(&mut self, key: U::D, value: U::D) {
         self.pending_writes.retain(|(k, _)| k != &key);
         self.pending_deletes.retain(|k| k != &key);
         // Remove from deletes if it was there
@@ -124,7 +126,7 @@ impl<M: Manifest, U: Unifier + Clone> DatabaseTransaction<M, U> {
     ///
     /// If a key is both written and deleted, the write takes precedence
     /// (so this delete will be ignored if the key was already written).
-    fn delete(&mut self, key: Vec<u8>) {
+    fn delete(&mut self, key: U::D) {
         // Only add to deletes if it's not already being written
         if !self.pending_writes.iter().any(|(k, _)| k == &key) {
             self.pending_deletes.push(key);
@@ -150,16 +152,16 @@ impl<M: Manifest, U: Unifier + Clone> DatabaseTransaction<M, U> {
     }
 
     /// Returns an iterator over the pending write operations.
-    pub fn pending_writes(&self) -> impl Iterator<Item = &Write> {
+    pub fn pending_writes(&self) -> impl Iterator<Item = &(U::D, U::D)> {
         self.pending_writes.iter()
     }
 
     /// Returns an iterator over the pending delete keys.
-    pub fn pending_deletes(&self) -> impl Iterator<Item = &Vec<u8>> {
+    pub fn pending_deletes(&self) -> impl Iterator<Item = &U::D> {
         self.pending_deletes.iter()
     }
 
-    fn serialization_config(&self) -> Configuration {
+    fn serialization_config(&self) -> U {
         self.serialization_config
     }
 
@@ -173,17 +175,21 @@ impl<M: Manifest, U: Unifier + Clone> DatabaseTransaction<M, U> {
     /// # Errors
     ///
     /// Returns a [`DatabaseError`] if the underlying batch operation fails.
-    pub fn commit<S: AtomicStorage>(
+    pub fn commit<S>(
         self,
         storage: &mut S,
-    ) -> Result<Vec<Option<Vec<u8>>>, DatabaseError<S::StoreError>> {
+    ) -> Result<Vec<Option<<S::Serializer as Unifier>::D>>, DatabaseError<S>>
+    where
+        S: AtomicStorage,
+        S: Storage<Serializer = U>,
+    {
         if self.is_empty() {
             return Ok(Vec::new());
         }
 
         // Convert to the format expected by batch_mixed
-        let inserts: Vec<Write> = self.pending_writes.into_iter().collect();
-        let removes: Vec<Vec<u8>> = self.pending_deletes.into_iter().collect();
+        let inserts = self.pending_writes.into_iter().collect();
+        let removes = self.pending_deletes.into_iter().collect();
 
         storage
             .batch_mixed(inserts, removes)
@@ -197,7 +203,12 @@ impl<M: Manifest, U: Unifier + Clone> DatabaseTransaction<M, U> {
         drop(self);
     }
 
-    pub fn consume(self) -> (impl Iterator<Item = Write>, impl Iterator<Item = Vec<u8>>) {
+    pub fn consume(
+        self,
+    ) -> (
+        impl Iterator<Item = (U::D, U::D)>,
+        impl Iterator<Item = U::D>,
+    ) {
         (
             self.pending_writes.into_iter(),
             self.pending_deletes.into_iter(),
@@ -208,9 +219,10 @@ impl<M: Manifest, U: Unifier + Clone> DatabaseTransaction<M, U> {
         &self,
         record: &R,
         key: &R::Key,
-    ) -> Result<Vec<Write>, U::SerError>
+    ) -> Result<Vec<(U::D, U::D)>, U::SerError>
     where
         R::Key: RecordKey<Record = R>,
+        SimpleIndexer<U>: crate::traits::Indexer<Error = U::SerError>,
     {
         let mut writes = Vec::with_capacity(R::INDEX_COUNT_HINT + 1);
 
@@ -218,20 +230,21 @@ impl<M: Manifest, U: Unifier + Clone> DatabaseTransaction<M, U> {
         record.index_keys(&mut indexer)?;
 
         for (discriminator, index_key) in indexer.into_index_keys() {
-            let mut entry = WrapPrelude::new::<R>(Subtable::Index(discriminator))
-                .to_bytes(self.serialization_config())?;
-            entry.extend_from_slice(&index_key);
+            let mut entry = self
+                .serialization_config()
+                .serialize(WrapPrelude::new::<R>(Subtable::Index(discriminator)))?;
+            entry.combine(index_key);
 
             // Indexes might be repeated, so we need to ensure that the key is unique.
             // TODO: Add a way to declare as unique and deduplicate by provided hash.
-            let key_bytes = encode_to_vec(key, self.serialization_config())?;
-            entry.extend_from_slice(&key_bytes);
+            let key_bytes = self.serialization_config().serialize(key)?;
+            entry.combine(key_bytes.clone());
 
-            writes.push((entry.clone(), key_bytes.clone()));
+            writes.push((entry, key_bytes));
         }
 
-        let key = wrap::<R>(key, self.serialization_config())?;
-        let value = encode_value(record, self.serialization_config())?;
+        let key = wrap::<R, U>(key, &self.serialization_config())?;
+        let value = self.serialization_config().serialize(record)?;
         writes.push((key, value));
 
         Ok(writes)
@@ -241,9 +254,10 @@ impl<M: Manifest, U: Unifier + Clone> DatabaseTransaction<M, U> {
         &self,
         record: &R,
         key: &R::Key,
-    ) -> Result<Vec<Vec<u8>>, U::SerError>
+    ) -> Result<Vec<U::D>, U::SerError>
     where
         R::Key: RecordKey<Record = R>,
+        SimpleIndexer<U>: crate::traits::Indexer<Error = U::SerError>,
     {
         let mut deletes = Vec::with_capacity(R::INDEX_COUNT_HINT + 1);
 
@@ -251,16 +265,17 @@ impl<M: Manifest, U: Unifier + Clone> DatabaseTransaction<M, U> {
         record.index_keys(&mut indexer)?;
 
         for (discriminator, index_key) in indexer.into_index_keys() {
-            let mut entry = WrapPrelude::new::<R>(Subtable::Index(discriminator))
-                .to_bytes(self.serialization_config())?;
-            entry.extend_from_slice(&index_key);
-            let key_bytes = encode_to_vec(key, self.serialization_config())?;
-            entry.extend_from_slice(&key_bytes);
+            let mut entry = self
+                .serialization_config()
+                .serialize(WrapPrelude::new::<R>(Subtable::Index(discriminator)))?;
+            entry.combine(index_key);
+            let key_bytes = self.serialization_config().serialize(key)?;
+            entry.combine(key_bytes);
 
             deletes.push(entry.clone());
         }
 
-        let key = wrap::<R, _>(key, self.serialization_config())?;
+        let key = wrap::<R, _>(key, &self.serialization_config())?;
         deletes.push(key);
 
         Ok(deletes)
