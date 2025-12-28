b89bd709af41c324d1a71fe15a8dbb3d1ba1a181
Mateusz Zając <matzayonc@gmail.com>
2025-12-28 13:36:14 +0100

    fix for string increment

--- kivis/src/traits/schema.rs
+++ kivis/src/traits/schema.rs
@@ -68,6 +68,7 @@ impl UnifierData for Vec<u8> {
     fn combine(&mut self, other: Self) {
         self.extend(other);
     }
+
     fn next(&mut self) {
         for i in (0..self.len()).rev() {
             // Add one if possible
@@ -92,16 +93,17 @@ impl UnifierData for alloc::string::String {
 
     fn next(&mut self) {
         let mut bytes = self.as_bytes().to_vec();
-        for i in (0..bytes.len()).rev() {
-            if bytes[i] < 255 {
-                bytes[i] += 1;
-                *self = alloc::string::String::from_utf8_lossy(&bytes).into_owned();
-                return;
+
+        let next_valid_string = loop {
+            bytes.next();
+
+            if let Ok(parsed_back) = alloc::string::String::from_utf8(bytes.clone()) {
+                // If the bytes are not valid UTF-8, increment and try again.
+                break parsed_back;
             }
-            bytes[i] = 0;
-        }
-        bytes.push(0);
-        *self = alloc::string::String::from_utf8_lossy(&bytes).into_owned();
+        };
+
+        *self = next_valid_string;
     }
 }
 
