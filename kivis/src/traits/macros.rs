6aae1686663fbbfd0e8a12b4164fb13d5b1496bc
Mateusz Zając <matzayonc@gmail.com>
2026-01-27 17:55:57 +0100

    minor traits reorganization

---
+++ kivis/src/traits/macros.rs
@@ -0,0 +1,179 @@
+/// Declarative macro to implement the Scope trait for multiple structs with a named manifest.
+/// Each struct gets its position in the array as its SCOPE value.
+/// Also generates an empty manifest struct and assigns it as the Manifest type.
+///
+/// # Example
+///
+/// ```rust
+/// use kivis::{manifest, Scope, Record};
+/// use serde::{Serialize, Deserialize};
+///
+/// #[derive(Record, Debug, Serialize, Deserialize)]
+/// struct User {
+///     id: u64,
+/// }
+///
+/// #[derive(Record, Debug, Serialize, Deserialize)]
+/// struct Post {
+///     id: u64,
+/// }
+///
+/// #[derive(Record, Debug, Serialize, Deserialize)]
+/// struct Comment {
+///     id: u64,
+/// }
+///
+/// #[derive(Record, Debug, Serialize, Deserialize)]
+/// struct Tag {
+///     id: u64,
+/// }
+///
+/// manifest![MyDatabase: User, Post, Comment, Tag];
+///
+/// // Verify the generated implementations
+/// assert_eq!(User::SCOPE, 0);
+/// assert_eq!(Post::SCOPE, 1);
+/// assert_eq!(Comment::SCOPE, 2);
+/// assert_eq!(Tag::SCOPE, 3);
+/// ```
+///
+/// # Compilation Errors
+///
+/// The macro requires a manifest name followed by a colon. Using the old syntax will fail:
+///
+/// ```compile_fail
+/// use kivis::manifest;
+///
+/// struct User;
+/// struct Post;
+///
+/// // This will fail to compile with the error:
+/// // "manifest! macro requires a manifest name followed by a colon. Use: manifest![ManifestName: Type1, Type2, ...]"
+/// manifest![User, Post];
+/// ```
+///
+/// Single type without colon also fails:
+///
+/// ```compile_fail
+/// use kivis::manifest;
+///
+/// struct User;
+///
+/// // This will fail to compile with the same error:
+/// // "manifest! macro requires a manifest name followed by a colon. Use: manifest![ManifestName: Type1, Type2, ...]"
+/// manifest![M: User];
+/// ```
+#[macro_export]
+macro_rules! manifest {
+    // Base case: empty list with manifest name
+    ($manifest_name:ident:) => {
+        #[derive(Default)]
+        pub struct $manifest_name;
+    };
+
+    // Multiple items case with manifest name - generate implementations with incrementing indices
+    ($manifest_name:ident: $($ty:ty),+ $(,)?) => {
+                $crate::paste! {
+                    #[derive(Default)]
+                    pub struct $manifest_name {
+                        $(
+                            [<last_ $ty:snake>]: ::core::option::Option<<$ty as $crate::DatabaseEntry>::Key>,
+                        )*
+                    }
+                }
+
+        $crate::scope_impl_with_index!($manifest_name, 0; $($ty),+);
+
+        impl $crate::Manifest for $manifest_name {
+            fn members() -> ::kivis::alloc::vec::Vec<u8> {
+                $crate::generate_member_scopes!(0; $($ty),+)
+            }
+
+            fn load<S: $crate::Storage>(&mut self, db: &mut $crate::Database<S, Self>) -> ::core::result::Result<(), $crate::DatabaseError<S>> {
+                $crate::paste! {
+                    $(
+                        self.[<last_ $ty:snake>] = ::core::option::Option::Some(db.last_id()?);
+                    )*
+                }
+                ::core::result::Result::Ok(())
+            }
+        }
+    };
+
+    // Error case: catch patterns without the required colon delimiter
+    ($($ty:ty),+ $(,)?) => {
+        compile_error!("manifest! macro requires a manifest name followed by a colon. Use: manifest![ManifestName: Type1, Type2, ...]");
+    };
+
+    // Error case: single type without colon
+    ($ty:ty) => {
+        compile_error!("manifest! macro requires a manifest name followed by a colon. Use: manifest![ManifestName: Type1, Type2, ...]");
+    };
+}
+
+/// Helper macro to generate member scope list
+#[macro_export]
+macro_rules! generate_member_scopes {
+    // Base case: no more types
+    ($index:expr;) => {
+        {
+            ::kivis::alloc::vec::Vec::new()
+        }
+    };
+
+    // Single type remaining
+    ($index:expr; $ty:ty) => {
+        {
+            ::kivis::alloc::vec::Vec::from([$index])
+        }
+    };
+
+    // Multiple types remaining - add current index and recurse
+    ($index:expr; $ty:ty, $($rest:ty),+) => {
+        {
+            let mut scopes = {
+                ::kivis::alloc::vec::Vec::from([$index])
+            };
+            scopes.extend($crate::generate_member_scopes!($index + 1; $($rest),+));
+            scopes
+        }
+    };
+}
+
+/// Helper macro for `scope_impl` that tracks the current index and manifest name
+#[macro_export]
+macro_rules! scope_impl_with_index {
+    // Base case: no more types
+    ($manifest_name:ident, $index:expr;) => {};
+
+    // Single type remaining
+    ($manifest_name:ident, $index:expr; $ty:ty) => {
+        impl $crate::Scope for $ty {
+            const SCOPE: u8 = $index;
+            type Manifest = $manifest_name;
+        }
+        impl $crate::Manifests<$ty> for $manifest_name {
+            fn last(&mut self) -> &mut ::core::option::Option<<$ty as $crate::DatabaseEntry>::Key> {
+                $crate::paste! {
+                    &mut self.[<last_ $ty:snake>]
+                }
+            }
+        }
+    };
+
+    // Multiple types remaining - implement for first and recurse
+    ($manifest_name:ident, $index:expr; $ty:ty, $($rest:ty),+) => {
+        impl $crate::Scope for $ty {
+            const SCOPE: u8 = $index;
+            type Manifest = $manifest_name;
+        }
+        impl $crate::Manifests<$ty> for $manifest_name {
+            fn last(&mut self) -> &mut ::core::option::Option<<$ty as $crate::DatabaseEntry>::Key> {
+                $crate::paste! {
+                    &mut self.[<last_ $ty:snake>]
+                }
+            }
+        }
+        $crate::scope_impl_with_index!($manifest_name, $index + 1; $($rest),+);
+    };
+}
---
+++ kivis/src/traits/unifier.rs
@@ -0,0 +1,223 @@
+use bincode::{
+    error::{DecodeError, EncodeError},
+    serde::{decode_from_slice, encode_to_vec},
+};
+
+#[cfg(not(feature = "std"))]
+use alloc::vec::Vec;
+
+use bincode::config::Configuration;
+use core::fmt::Debug;
+use serde::{de::DeserializeOwned, Serialize};
+
+pub trait UnifierData {
+    /// The owned type for this data (e.g., Vec<u8> for [u8], String for str)
+    type Owned: Default + Clone + AsRef<Self>;
+
+    /// Increments the buffer to the next value.
+    fn next(buffer: &mut Self::Owned);
+
+    /// Appends a single part to the buffer.
+    fn extend(buffer: &mut Self::Owned, part: &Self);
+
+    /// Returns the current length of the buffer.
+    fn len(buffer: &Self::Owned) -> usize;
+
+    /// Extracts a range from the owned buffer as a reference.
+    /// This is used to extract individual values from buffered data.
+    #[must_use]
+    fn extract_range(buffer: &Self::Owned, start: usize, end: usize) -> &Self;
+
+    /// Converts a reference to an owned value.
+    #[must_use]
+    fn to_owned(data: &Self) -> Self::Owned;
+
+    /// Duplicates data by converting to owned.
+    #[must_use]
+    fn duplicate(data: &Self) -> Self::Owned {
+        Self::to_owned(data)
+    }
+}
+
+impl UnifierData for [u8] {
+    type Owned = Vec<u8>;
+
+    fn next(buffer: &mut Self::Owned) {
+        for i in (0..buffer.len()).rev() {
+            // Add one if possible
+            if buffer[i] < 255 {
+                buffer[i] += 1;
+                return;
+            }
+            // Otherwise, set to zero and carry over
+            buffer[i] = 0;
+        }
+
+        // If all bytes were 255, we need to add a new byte
+        buffer.push(0);
+    }
+
+    fn extend(buffer: &mut Self::Owned, part: &Self) {
+        buffer.extend_from_slice(part);
+    }
+
+    fn len(buffer: &Self::Owned) -> usize {
+        buffer.len()
+    }
+
+    fn extract_range(buffer: &Self::Owned, start: usize, end: usize) -> &Self {
+        &buffer[start..end]
+    }
+
+    fn to_owned(data: &Self) -> Self::Owned {
+        data.to_vec()
+    }
+}
+
+#[cfg(feature = "std")]
+impl UnifierData for str {
+    type Owned = alloc::string::String;
+
+    fn next(buffer: &mut Self::Owned) {
+        let mut bytes = buffer.as_bytes().to_vec();
+
+        let next_valid_string = loop {
+            <[u8]>::next(&mut bytes);
+
+            if let Ok(parsed_back) = alloc::string::String::from_utf8(bytes.clone()) {
+                // If the bytes are not valid UTF-8, increment and try again.
+                break parsed_back;
+            }
+        };
+
+        *buffer = next_valid_string;
+    }
+
+    fn extend(buffer: &mut Self::Owned, part: &Self) {
+        buffer.push_str(part);
+    }
+
+    fn len(buffer: &Self::Owned) -> usize {
+        buffer.len()
+    }
+
+    fn extract_range(buffer: &Self::Owned, start: usize, end: usize) -> &Self {
+        &buffer[start..end]
+    }
+
+    fn to_owned(data: &Self) -> Self::Owned {
+        data.to_string()
+    }
+}
+
+pub trait Unifiable: Serialize + DeserializeOwned {}
+pub trait UnifiableRef: Unifiable + Clone {}
+
+impl<T: Serialize + DeserializeOwned> Unifiable for T {}
+impl<T: Serialize + DeserializeOwned + Clone> UnifiableRef for T {}
+
+pub trait Unifier {
+    type K: UnifierData + ?Sized;
+    type V: UnifierData + ?Sized;
+    type SerError: Debug;
+    type DeError: Debug;
+
+    /// Serializes a key directly into an existing buffer and returns the start and end positions.
+    /// # Errors
+    ///
+    /// Returns an error if serialization fails.
+    fn serialize_key(
+        &self,
+        buffer: &mut <Self::K as UnifierData>::Owned,
+        data: impl Unifiable,
+    ) -> Result<(usize, usize), Self::SerError>;
+
+    /// Serializes a borrowed key directly into an existing buffer and returns the start and end positions.
+    /// # Errors
+    ///
+    /// Returns an error if serialization fails.
+    fn serialize_key_ref<R: UnifiableRef>(
+        &self,
+        buffer: &mut <Self::K as UnifierData>::Owned,
+        data: &R,
+    ) -> Result<(usize, usize), Self::SerError> {
+        self.serialize_key(buffer, data.clone())
+    }
+
+    /// Serializes a value directly into an existing buffer and returns the start and end positions.
+    /// # Errors
+    ///
+    /// Returns an error if serialization fails.
+    fn serialize_value(
+        &self,
+        buffer: &mut <Self::V as UnifierData>::Owned,
+        data: impl Unifiable,
+    ) -> Result<(usize, usize), Self::SerError>;
+
+    /// Serializes a borrowed value directly into an existing buffer and returns the start and end positions.
+    /// # Errors
+    ///
+    /// Returns an error if serialization fails.
+    fn serialize_value_ref<R: UnifiableRef>(
+        &self,
+        buffer: &mut <Self::V as UnifierData>::Owned,
+        data: &R,
+    ) -> Result<(usize, usize), Self::SerError> {
+        self.serialize_value(buffer, data.clone())
+    }
+
+    /// Deserializes a key from the given data.
+    /// # Errors
+    ///
+    /// Returns an error if deserialization fails.
+    fn deserialize_key<T: Unifiable>(
+        &self,
+        data: &<Self::K as UnifierData>::Owned,
+    ) -> Result<T, Self::DeError>;
+
+    /// Deserializes a value from the given data.
+    /// # Errors
+    ///
+    /// Returns an error if deserialization fails.
+    fn deserialize_value<T: Unifiable>(
+        &self,
+        data: &<Self::V as UnifierData>::Owned,
+    ) -> Result<T, Self::DeError>;
+}
+
+impl Unifier for Configuration {
+    type K = [u8];
+    type V = [u8];
+    type SerError = EncodeError;
+    type DeError = DecodeError;
+
+    fn serialize_key(
+        &self,
+        buffer: &mut Vec<u8>,
+        data: impl Serialize,
+    ) -> Result<(usize, usize), Self::SerError> {
+        let start = <[u8]>::len(buffer);
+        let serialized = encode_to_vec(data, Self::default())?;
+        <[u8]>::extend(buffer, &serialized);
+        Ok((start, <[u8]>::len(buffer)))
+    }
+
+    fn serialize_value(
+        &self,
+        buffer: &mut Vec<u8>,
+        data: impl Serialize,
+    ) -> Result<(usize, usize), Self::SerError> {
+        let start = <[u8]>::len(buffer);
+        let serialized = encode_to_vec(data, Self::default())?;
+        <[u8]>::extend(buffer, &serialized);
+        Ok((start, <[u8]>::len(buffer)))
+    }
+
+    fn deserialize_key<T: DeserializeOwned>(&self, data: &Vec<u8>) -> Result<T, Self::DeError> {
+        Ok(decode_from_slice(data, Self::default())?.0)
+    }
+
+    fn deserialize_value<T: DeserializeOwned>(&self, data: &Vec<u8>) -> Result<T, Self::DeError> {
+        Ok(decode_from_slice(data, Self::default())?.0)
+    }
+}
